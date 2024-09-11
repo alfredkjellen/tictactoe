750db6d4b365e15dab8049aca656cf3114ef34b8
3JPiPi <jens.j.jansson@gmail.com>
2024-09-11 14:01:06 +0200

    Update LED_management.py
    Added comments to codfe

--- LED_management.py
+++ LED_management.py
@@ -1,11 +1,18 @@
+# add either readme or short explaination what this file does or server purpose
+# all funcitons need function documentation
+# all function ideally have some return (true or false)  to say if ok or not
 import RPi.GPIO as GPIO
 import threading
 from time import sleep
 
-GPIO.setmode(GPIO.BCM) 
-GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)  # Jens what is this please explain
+GPIO.setwarnings(False) # Jens what is this please explain
 
-stop_button_pin = [20, 21, 27]
+# Jens make a ascii art drawing to explain ping 
+# x x x
+# y y y
+# zzz
+stop_button_pin = [20, 21, 27] 
 led_pins = [2, 3, 4, 7, 6, 5, 8, 9, 10]
 
 GPIO.setup(stop_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -14,7 +21,7 @@ for pin in led_pins:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
 
-
+# jens put these "globals" on top and add explaination
 stop_animation = False
 
 
@@ -22,28 +29,28 @@ def reset():
     for pin in led_pins:
         GPIO.output(pin, GPIO.LOW)
 
-
 def turn_on(led_pin):
     GPIO.output(led_pin, GPIO.HIGH)
 
 def turn_off(led_pin):
     GPIO.output(led_pin, GPIO.LOW)
 
-def main_animation():
-    global stop_animation
+def main_animation(): # too big of a funciton max 10-20 rows
+    global stop_animation # usuall good to try to avoid globals but I think its needed.. but u can pass it in instead
     while not stop_animation:
-        
+
+        # jens function in a function is nok :) for me or does it needs to be like this
         def roulette():
             for pin in led_pins:
                 if stop_animation:
                     break
                 turn_on(pin)
-                sleep(0.1)
+                sleep(0.1) # ejensja no magic number proper ROULETTE_SLEEP_TIME_IN_SECONDS = 0.1
                 turn_off(pin)
             sleep(0.1)
             
         def snake():
-            for i in range(2,11):
+            for i in range(2,11): # no magic numbers
                 if stop_animation:
                     break
                 turn_on(i)
@@ -52,7 +59,7 @@ def main_animation():
             
             
         def explosion():
-            turn_on(6)
+            turn_on(6) # ejensja put it in some constant list [6,0,3,4,,1,2,,12,2] where 0 means sleep
             sleep(0.1)
             turn_on(3)
             turn_on(5)
@@ -90,7 +97,8 @@ def main_animation():
             sleep(0.1)
 
         def blink():
-            def bling(start, finnish):
+            #avoid functions within functions
+            def bling(start, finnish): # blink bling I dont understand this part
                 for i in range(start, finnish, 2):
                     if stop_animation:
                         break
@@ -104,11 +112,11 @@ def main_animation():
             for i in range(4):
                 if stop_animation:
                         break
-                bling(2, 11)
+                bling(2, 11) # no magic numbers define at top or makea separate file with config or somethinbg
                 bling(3, 10)    
             sleep(0.1)
 
-        def clock():
+        def clock(): # dont understand this
             def pointer(start, finnish, step):
                  for i in range(start , finnish, step):
                         if stop_animation:
@@ -127,7 +135,7 @@ def main_animation():
             sleep(0.1)
 
                 
-        def tic_tac_toe():
+        def tic_tac_toe(): # this one is rather clear but not function in funion
             T = [6, 9, 2, 3, 4]
             I = [3, 6, 9]
             C = [4, 3, 2, 5, 8, 9, 10]
@@ -157,7 +165,7 @@ def main_animation():
             
         animations = [explosion, roulette, glitter, snake, blink, clock, tic_tac_toe]
         
-        for a in animations:
+        for a in animations: # dont use a() use something descriptive
             a()
             reset()
 
